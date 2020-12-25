6c2fb5712ee158029eda66d25fffa7b45f85dd2b
KallNascimento <iksn7@icloud.com>
2020-12-25 12:53:50 -0300

    services and interfaces add

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { CourseComponent } from './course/course.component';
+import { StudentComponent } from './student/student.component';
+import { SchoolClassComponent } from './school-class/school-class.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    StudentComponent,
+    SchoolClassComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
