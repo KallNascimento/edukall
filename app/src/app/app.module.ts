46d3154fcea684c7cbfea5b4c68babba6a549ca9
KallNascimento <iksn7@icloud.com>
2021-01-03 12:55:01 -0300

    dropdown add

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,23 +1,21 @@
+import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CourseComponent } from './components/course/course.component';
 import { StudentComponent } from './components/student/student.component';
 import { SchoolClassComponent } from './components/school-class/school-class.component';
-
-
-
+import { HomeComponent } from './components/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
     StudentComponent,
-    SchoolClassComponent
+    SchoolClassComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
